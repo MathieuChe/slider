1aaa9a3c587332e6d84514bccd0da2924a8813ab
Mathieu Chelim <mathieu.chelim@gmail.com>
2020-12-18 17:36:02 +0100

    Update

--- main.js
+++ main.js
@@ -27,15 +27,19 @@ function showSlides(n) {
     slideIndex = slides.length;
   }
 
+  // Boucle permettant de cacher la slide en fonction de son index et modifie l'attribut display en "none"
   for (let i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
 
+  // Boucle permettant de remplacer la class du dot qui est "" à active 
   for (let i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
 
+  // Comme slideIndex est assigné à 1, on met slideIndex -1 pour commencer par l'index 0 vue que nous commencons par 0. Afficher la slide en question. 
   slides[slideIndex - 1].style.display = "block";
 
+  // Ajouter la classe active
   dots[slideIndex - 1].className += " active";
 }
